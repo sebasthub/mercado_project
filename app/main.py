7f82cd707d01c5cf8b1172ae4d84ae9b9caa1cdf
sebasthub <playandroidhb@gmail.com>
2024-09-16 23:10:09 -0300

    teste cors 5

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sebasthub.github.io"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],
